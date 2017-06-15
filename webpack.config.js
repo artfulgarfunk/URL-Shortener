73aed89c16a7b92906a104d477085e3ca3b878d7
Jack <92jackhenderson@gmail.com>
2017-06-15 16:29:28 +0100

    Added test suite for TDD
    - Install mocha
    - Install jsdom
    - Install webpack dev server
    - Install enzyme
    - Install chai
    - Add new scripts for test,dev:hot to package.json
    - Create test/helpers/browser.js for test browser simulation

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,12 @@ module.exports = {
   entry: './src/App.jsx',
   output: {
     path: __dirname + '/static',
-    filename: 'app.bundle.js'
+    filename: 'app.bundle.js',
+  },
+  externals: {
+    'cheerio': 'window',
+    'react/lib/ExecutionEnvironment': true,
+    'react/lib/ReactContext': true,
   },
   module: {
     loaders: [
